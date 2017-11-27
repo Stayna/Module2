4e1a06ed572bf8c1cf416cda68ed165dcb737b52
Anna Novikova <anna_novikova@epam.com>
2017-11-27 15:36:34 +0300

    new file

---
+++ Calc/ConsoleApplication1/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Math
+{
+    public string myMath()
+    {
+        oper = Convert.ToChar(Console.ReadLine());
+        {
+            switch (oper)
+            {
+                case "+": a + b; return;
+                case "-": (a - b); return;
+                case "*": (a * b); return;
+                case "/": (a / b); return;
+            }
+        }
+    }
+}
---
+++ Calc/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b;
+            double c;
+            string oper;
+            Program calculation = new Program();
+            Console.WriteLine("Введите первое значение");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите опертатор");
+            oper = Console.ReadLine(); ;
+            Console.WriteLine("Введите второе значение");
+            b = Convert.ToDouble(Console.ReadLine());
+            
+            switch (oper)
+            {
+                case "+": c = a + b;
+                    Console.WriteLine(c);
+                    break;
+                case "-": c = (a - b);
+                    Console.WriteLine(c);
+                    break;
+                case "*": c = (a * b); 
+                    Console.WriteLine(c);
+                    break;
+                case "/": c = (a / b); 
+                    Console.WriteLine(c);
+                    break;
+            }
+            Console.ReadLine();          
+        }
+    }
+}
